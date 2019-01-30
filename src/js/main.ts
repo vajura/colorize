346f11e508c3de2126c456c3c76f1cec31af5ec9
tomaz turner <t.turner@netconomy.net>
2019-01-30 21:03:21 +0100

    added image reader

--- src/js/main.ts
+++ src/js/main.ts
@@ -13,53 +13,78 @@ class Colorize {
 	private ctx: any;
 	private imageData: any;
 	private data32: Uint32Array;
+	private imagesData32: Uint32Array[] = [];
 
 	private instances: ColorizeInstance[] = [];
 
-	constructor(options: ColorizeOptions) {
-		const domRect: any = document.getElementById(options.domId);
-		(<HTMLCanvasElement>document.getElementById(options.domId)).height = Math.round(domRect.clientHeight);
-		(<HTMLCanvasElement>document.getElementById(options.domId)).width = Math.round(domRect.clientWidth);
-		this.domWidth = Math.round(domRect.clientWidth);
-		this.domHeight = Math.round(domRect.clientHeight);
-
-		this.canv = <HTMLCanvasElement>document.getElementById(options.domId);
-	  this.ctx = this.canv.getContext('2d');
-		this.imageData = this.ctx.createImageData(this.domWidth, this.domHeight);
-		this.data32 = new Uint32Array(this.imageData.data.buffer);
-
-		this.generateMoveArray();
-
-		for (let a = 0; a < options.palettes.length; a++) {
-      this.colors[a] = this.generateColorArray(options.palettes[a]);
-		}
-		for (let a = 0; a < options.startingPoints.length; a++) {
-			const sp: StartingPoint = options.startingPoints[a];
-			const instance: ColorizeInstance = {};
-			instance.xPos = sp.startingX || 1;
-			instance.yPos = sp.startingY || 1;
-			if (sp.overrideStartToCenter) {
-				instance.xPos = Math.round(this.domWidth / 2);
-				instance.yPos = Math.round(this.domHeight / 2);
-			}
-			instance.speed = sp.speed || 100;
-			instance.selectedPalette = sp.palette || 0;
-			instance.pixel1dField = [];
-			instance.interval = 1000 / 50;
-			instance.randArray = options.randArray && options.randArray.length ? options.randArray : [7];
-			instance.counter = 0;
-			instance.pixel1dOldLength = 0;
-			instance.genDone = false;
-			this.instances.push(instance);
-		}
-		this.resetBoardToNextGen();
-		this.nextGen();
-		for (let a = 0; a < this.instances.length; a++) {
-			this.draw(this.instances[a]);
-		}
+	private async loadImage(image: any): Promise<Uint32Array> {
+    return new Promise((resolve, reject) => {
+      image.onload = () => {
+        const canvas = <HTMLCanvasElement>document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.getContext('2d').drawImage(image, 0, 0);
+        // this.imagesData32[a] = new Uint32Array(ctx.getImageData(0, 0, this.domWidth, this.domHeight).data.buffer);
+        resolve(new Uint32Array(ctx.getImageData(0, 0, this.domWidth, this.domHeight).data.buffer));
+      };
+      image.onError = reject;
+    });
 	}
 
-	nextGen = () => {
+	private async construct(options: ColorizeOptions, imageURLs: string[]) {
+    const domRect: any = document.getElementById(options.domId);
+    this.domHeight = Math.round(domRect.clientHeight);
+    this.domWidth = Math.round(domRect.clientWidth);
+    domRect.height = this.domHeight;
+    domRect.width = this.domWidth;
+
+    this.canv = <HTMLCanvasElement>document.getElementById(options.domId);
+    this.ctx = this.canv.getContext('2d');
+    this.imageData = this.ctx.createImageData(this.domWidth, this.domHeight);
+    this.data32 = new Uint32Array(this.imageData.data.buffer);
+
+    for (let a = 0; a < imageURLs.length; a++) {
+      const image = new Image();
+      image.setAttribute('crossOrigin', '');
+      image.src = imageURLs[a];
+			this.imagesData32[a] = await this.loadImage(image);
+      console.log(this.imagesData32[a].length);
+    }
+    console.log('after');
+    this.generateMoveArray();
+
+    for (let a = 0; a < options.palettes.length; a++) {
+      this.colors[a] = this.generateColorArray(options.palettes[a]);
+    }
+    for (let a = 0; a < options.startingPoints.length; a++) {
+      const sp: StartingPoint = options.startingPoints[a];
+      const instance: ColorizeInstance = {};
+      instance.xPos = sp.startingX || 1;
+      instance.yPos = sp.startingY || 1;
+      if (sp.overrideStartToCenter) {
+        instance.xPos = Math.round(this.domWidth / 2);
+        instance.yPos = Math.round(this.domHeight / 2);
+      }
+      instance.speed = sp.speed || 100;
+      instance.selectedPalette = sp.palette || 0;
+      instance.pixel1dField = [];
+      instance.interval = 1000 / 50;
+      instance.randArray = options.randArray && options.randArray.length ? options.randArray : [7];
+      instance.counter = 0;
+      instance.pixel1dOldLength = 0;
+      instance.genDone = false;
+      this.instances.push(instance);
+    }
+    this.resetBoardToNextGen();
+    /*this.nextGen();
+    for (let a = 0; a < this.instances.length; a++) {
+      this.draw(this.instances[a]);
+    }*/
+	}
+	constructor(options: ColorizeOptions, imageURLs: string[]) {
+		this.construct(options, imageURLs);
+	}
+
+	private nextGen() {
 		let next = true;
 		for (let a = 0; a < this.instances.length; a++) {
 			if (!this.instances[a].genDone) {
@@ -85,9 +110,9 @@ class Colorize {
 		requestAnimationFrame(() => {
 			this.nextGen();
     });
-	};
+	}
 
-	draw = (instance: ColorizeInstance) => {
+	private draw(instance: ColorizeInstance) {
 		if (!instance.genDone) {
 			instance.pixel1dOldLength = instance.pixel1dField.length;
 			for (let b = 0; b < instance.speed; b++) {
@@ -119,7 +144,7 @@ class Colorize {
         this.draw(instance);
       });
 		}
-	};
+	}
 
 	private resetBoardToNextGen(initArray: boolean = true) {
 		this.currentGen++;
@@ -163,12 +188,10 @@ class Colorize {
 
 	private interpolateColor(color1: any, color2: any, factor: number = 0.5): number {
     const numberArray: number[] = color1.slice();
-    let colorNumber: number = 0;
     for (let a = 0; a < 3; a++) {
         numberArray[a] = Math.round(numberArray[a] + factor * (color2[a] - color1[a]));
     }
-    colorNumber = 0xFF000000 + numberArray[0] + numberArray[1] * 256 + numberArray[2] * 256 * 256;
-    return colorNumber;
+    return 0xFF000000 + numberArray[0] + numberArray[1] * 256 + numberArray[2] * 256 * 256;
 	}
 
 	private interpolateColors(color1: any, color2: any, steps: number): number[] {
@@ -201,5 +224,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 			speed: 100,
 			palette: 0
     }] as StartingPoint[]
-	} as ColorizeOptions);
+	} as ColorizeOptions,
+		['https://i.imgur.com/ZjkfV2J.png', 'https://i.imgur.com/PJAzXYz.png', 'https://i.imgur.com/NcZjxum.png']);
 });
